53c1defcaba7f787a019ea75c686bda91560675d
Wato-Adano <watobuchaadano@gmail.com>
2024-04-08 19:17:17 +0300

    add file

--- binary.py
+++ binary.py
@@ -4,9 +4,9 @@ class TreeNode:
         self.left = None
         self.right = None
 
-def inOrderTraversal(node):
-    if node is None:
-        return
+    def inOrderTraversal(node):
+        if node is None:
+            return
     inOrderTraversal(node.left)
     print(node.data, end=", ")
     inOrderTraversal(node.right)
@@ -32,3 +32,44 @@ nodeB.right = nodeF
 nodeF.left = nodeG
  
 inOrderTraversal(root)
+  
+  
+class newNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
+ 
+
+
+ 
+ 
+def findMax(root):
+ 
+              if (root == None):
+                  return float('-inf')
+ 
+   
+res = root.data
+lres = findMax(root.left)
+rres = findMax(root.right)
+if (lres > res):
+        res = lres
+if (rres > res):
+        res = rres
+     return res
+ 
+ 
+if __name__ == '__main__':
+    root = newNode(2)
+    root.left = newNode(7)
+    root.right = newNode(5)
+    root.left.right = newNode(6)
+    root.left.right.left = newNode(1)
+    root.left.right.right = newNode(11)
+    root.right.right = newNode(9)
+    root.right.right.left = newNode(4)
+ 
+
+    print("Maximum element is",
+          findMax(root))
+ 
